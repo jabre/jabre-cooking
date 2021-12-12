e0b5de51c5e6355933926a613fdff824bdab1347
jabre <marko13r@gmail.com>
2021-12-12 19:20:06 +0100

    Initial spring boot project

---
+++ src/main/java/com/jabre/cooking/JabreCookingArtsApplication.java
@@ -0,0 +1,13 @@
+package com.jabre.cooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabreCookingArtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabreCookingArtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabre/cooking/JabreCookingArtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabre.cooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabreCookingArtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
